0332a2d4069d6563e0725aee7c0f8699d57d0bcd
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2025-08-14 16:23:21 -0300

    Começando método de média de tempo de acesso

--- AcademiaDoZe.Infraestrutura/Repositories/AcessoRepository.cs
+++ AcademiaDoZe.Infraestrutura/Repositories/AcessoRepository.cs
@@ -135,7 +135,7 @@ namespace AcademiaDoZe.Infraestrutura.Repositories
 
         public async Task<Dictionary<TimeOnly, int>> GetHorarioMaisProcuradoPorMes(int mes)
         {
-            if (mes <= 0) throw new InvalidOperationException("O número de dias deve ser maior que zero" + nameof(mes));
+            if (mes <= 0) throw new InvalidOperationException("O número do mês deve ser maior que zero" + nameof(mes));
             await using var connection = await GetOpenConnectionAsync();
             //Falta filtrar por mês
             string query = $"SELECT TOP 1 " +
@@ -152,8 +152,20 @@ namespace AcademiaDoZe.Infraestrutura.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<Dictionary<int, TimeSpan>> GetPermanenciaMediaPorMes(int mes)
+        public async Task<Dictionary<int, TimeSpan>> GetPermanenciaMediaPorMes(int mes)
         {
+            if (mes <= 0) throw new InvalidOperationException("O número do mês deve ser maior que zero" + nameof(mes));
+            await using var connection = await GetOpenConnectionAsync();
+
+            string query = $"SELECT pessoa_id, data_hora FROM {TableName}"
+                + $"WHERE MONTH(data_hora) = @Mes";
+            await using var commando = DbProvider.CreateCommand(query, connection);
+            commando.Parameters.Add(DbProvider.CreateParameter("@Mes", mes, DbType.Int32, _databaseType));
+            await using var reader = await commando.ExecuteReaderAsync();
+            decimal TotalHotas = 0;
+            int totalAcessos = 0;
+
+            //Finalizar
             throw new NotImplementedException();
         }
 

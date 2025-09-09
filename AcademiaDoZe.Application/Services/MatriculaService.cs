b0883bc7916fd5b18206fe5e2c76255591f1d504
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2025-09-09 08:33:57 -0300

    Gerando commit para poder realizar git pull

---
+++ AcademiaDoZe.Application/Services/MatriculaService.cs
@@ -0,0 +1,57 @@
+﻿using AcademiaDoZe.Application.DTOs;
+using AcademiaDoZe.Application.Interfaces;
+using AcademiaDoZe.Application.Mappings;
+using AcademiaDoZe.Domain.Repositories;
+using AcademiaDoZe.Application.Security;
+
+namespace AcademiaDoZe.Application.Services
+{
+    public class MatriculaService : IMatriculaService
+    {
+        private readonly Func<IMatriculaService> _repoFactory;
+        public MatriculaService(Func<IMatriculaService> repoFactory)
+        {
+            _repoFactory = repoFactory ?? throw new ArgumentNullException(nameof(repoFactory));
+        }
+
+        public Task<MatriculaDTO> AdicionarAsync(MatriculaDTO matriculaDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MatriculaDTO> AtualizarAsync(MatriculaDTO matriculaDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatriculaDTO>> ObterAtivasAsync(int alunoId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatriculaDTO>> ObterPorAlunoIdAsync(int alunoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MatriculaDTO> ObterPorIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatriculaDTO>> ObterTodasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatriculaDTO>> ObterVencendoEmDiasAsync(int dias)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoverAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

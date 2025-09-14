19c251390441eb65ab8d8b3f4687bb51f38fb3a7
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2025-09-13 21:38:14 -0300

    Criando Viws e implementando a DashboardListPage.xaml

---
+++ AcademiaDoZe.Presentation.AppMaui/Views/LogradouroListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AcademiaDoZe.Presentation.AppMaui.Views;
+
+public partial class LogradouroListPage : ContentPage
+{
+	public LogradouroListPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AcademiaDoZe.Presentation.AppMaui/Views/LogradouroPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AcademiaDoZe.Presentation.AppMaui.Views;
+
+public partial class LogradouroPage : ContentPage
+{
+	public LogradouroPage()
+	{
+		InitializeComponent();
+	}
+}

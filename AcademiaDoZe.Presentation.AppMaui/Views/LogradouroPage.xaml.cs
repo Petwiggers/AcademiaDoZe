fb2f914bdf04f7647dacf006469a0be1393621a1
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2025-09-14 08:49:40 -0300

    Implementando LogradouroViewPage

--- AcademiaDoZe.Presentation.AppMaui/Views/LogradouroPage.xaml.cs
+++ AcademiaDoZe.Presentation.AppMaui/Views/LogradouroPage.xaml.cs
@@ -1,9 +1,14 @@
+using AcademiaDoZe.Presentation.AppMaui.ViewModels;
 namespace AcademiaDoZe.Presentation.AppMaui.Views;
-
 public partial class LogradouroPage : ContentPage
 {
-	public LogradouroPage()
-	{
-		InitializeComponent();
-	}
+    public LogradouroPage(LogradouroViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        /* implementar depois */
+    }
 }

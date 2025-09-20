fdadac60c48676189bfa3064c3fc29e78778f3e8
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2025-09-20 17:57:13 -0300

    Adicionando arquivo de fonte de ícones e referenciando no projeto

--- AcademiaDoZe.Presentation.AppMaui/MauiProgram.cs
+++ AcademiaDoZe.Presentation.AppMaui/MauiProgram.cs
@@ -15,6 +15,8 @@ namespace AcademiaDoZe.Presentation.AppMaui
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                // Adicione esta linha para registrar a fonte
+                fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
             });
 
             // Configurar serviços da aplicação e repositórios
